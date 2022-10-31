657acc464b6b1d440597bdcafb3b73980d2347d8
Oh Wow Marketing Solutions <imarkdesigns@gmail.com>
2022-10-31 03:50:19 -0700

    Updates for Privacy Policy

--- views/pages/legal.php
+++ views/pages/legal.php
@@ -1,1 +1,19 @@
-<?php the_title(); ?>
+<header data-fragment="hero" class="uk-flex uk-flex-center uk-flex-middle uk-height-medium">
+    <article class="uk-text-center uk-width-xlarge">
+        <?php the_field( 'hdr_content' ); ?>
+    </article>
+</header>
+
+<main id="main" class="main" role="main">
+
+    <section class="legal-information | uk-section uk-section-muted">
+        <div class="uk-container uk-container-small">
+
+            <article class="uk-article">
+                <?php the_content(); ?>
+            </article>
+
+        </div>
+    </section>
+
+</main>
