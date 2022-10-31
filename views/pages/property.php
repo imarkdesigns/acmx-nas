2b144101516e0bee76ddb70178c1efcfb166a9ee
Oh Wow Marketing Solutions <imarkdesigns@gmail.com>
2022-10-31 00:22:14 -0700

    Setup Property Management

--- views/pages/property.php
+++ views/pages/property.php
@@ -1,1 +1,26 @@
-<?php the_title(); ?>
+<?php
+// Loan Maturity Header Settings
+$pm_hdr_content = get_field( 'hdr_content' );
+$pm_hdr_bg = get_field( 'hdr_photo' );
+
+?>
+<header data-fragment="hero" class="loan-maturity | uk-flex uk-flex-middle uk-light" data-src="<?php echo ( $pm_hdr_bg ) ? $pm_hdr_bg['url'] : '//placem.at/places?w=1920&h=550&txt=0&random=1' ?>" uk-img>
+    <div class="uk-width-1-1">
+        <div class="uk-container uk-container-small">
+            <?php echo $pm_hdr_content; ?>
+        </div>
+    </div>
+</header>
+
+<main id="main" class="main" role="main">
+
+    <section class="loan-intro | uk-section">
+        <div class="uk-container uk-container-small">
+            <?php the_content(); ?>
+        </div>
+    </section>
+
+    <?php
+    get_template_part( _opt.'ondemand-banner' ); ?>
+    
+</main>
