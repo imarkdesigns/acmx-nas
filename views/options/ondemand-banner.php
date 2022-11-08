279023e7195a2a4e39c06bd91ffabd799c054280
Oh Wow Marketing Solutions <imarkdesigns@gmail.com>
2022-11-09 03:54:22 +0800

    Update OnDemand Banner Page Visibility

--- views/options/ondemand-banner.php
+++ views/options/ondemand-banner.php
@@ -1,7 +1,9 @@
 <?php
 // Fragment: OnDemand Banner
 $od_banner = get_field( 'ondemand_bg', 'option' );
+$od_visibility = get_field( 'ondemand_visibility', 'option' );
 
+if ( is_page( $od_visibility ) ) :
 ?>
 <section class="ondemand-banner | uk-section uk-position-relative uk-padding-remove" data-src="<?php echo ( $od_banner ) ? $od_banner : _uri.'/resources/images/img-ondemand_bg.jpg'; ?>" uk-img>
     <div class="uk-overlay uk-position-center-right uk-position-medium uk-width-2-3@s uk-width-1-2@m">
@@ -10,3 +12,4 @@ $od_banner = get_field( 'ondemand_bg', 'option' );
         <?php the_field( 'ondemand_details', 'option' ); ?>
     </div>
 </section>
+<?php endif;
