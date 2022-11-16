8bfb4d10687f6a0c01c63ded084b1950c52f0ce0
Oh Wow Marketing Solutions <imarkdesigns@gmail.com>
2022-11-17 05:19:04 +0800

    Temporary fix to counterup

--- resources/scripts/route/about.js
+++ resources/scripts/route/about.js
@@ -11,18 +11,43 @@
             // Profile
             jQuery('#bio .uk-article').find('p:first').addClass('uk-dropcap');
 
-            $url = window.location.origin;
-            jQuery.getScript('https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.0/jquery.waypoints.min.js', function() {
-                jQuery.getScript($url + '/wp-content/themes/acmx-nas/resources/scripts/counterup.min.js', function() {
-                    jQuery('.counter').counterUp({
-                        delay: 50,
-                        time: 1500,
-                    });
-                });
-            });
+            // $url = window.location.origin;
+            // jQuery.getScript('https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.0/jquery.waypoints.min.js', function() {
+            //     jQuery.getScript($url + '/wp-content/themes/acmx-nas/resources/scripts/counterup.min.js', function() {
+            //         jQuery('.counter').counterUp({
+            //             delay: 50,
+            //             time: 1500,
+            //         });
+            //     });
+            // });
 
         }    
     }, 100);
 
 })(jQuery);
 
+
+
+    jQuery.getScript('https://unpkg.com/counterup2@2.0.2/dist/index.js', function() {
+
+        const counterUp = window.counterUp.default
+
+        const callback = entries => {
+            entries.forEach( entry => {
+                const el = entry.target
+                if ( entry.isIntersecting && ! el.classList.contains( 'is-visible' ) ) {
+                    counterUp( el, {
+                        duration: 2000,
+                        delay: 16,
+                    } )
+                    el.classList.add( 'is-visible' )
+                }
+            } )
+        }
+
+        const IO = new IntersectionObserver( callback, { threshold: 1 } )
+
+        const el = document.querySelector( '.counter' )
+        IO.observe( el )
+
+    });
