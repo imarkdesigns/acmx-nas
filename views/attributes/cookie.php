347c82a5b8ab4d444a9ca658512e3f455dc95873
Oh Wow Marketing Solutions <imarkdesigns@gmail.com>
2022-11-30 05:00:56 +0800

    change data attributes

--- views/attributes/cookie.php
+++ views/attributes/cookie.php
@@ -1,4 +1,4 @@
-<div class="uk-alert-primary uk-position-bottom accept-cookies" hidden uk-alert tabindex="0" data-router=cookie>
+<div class="uk-alert-primary uk-position-bottom accept-cookies" hidden uk-alert tabindex="0" data-fragment="cookie">
     <div class="content">
         <a class="uk-alert-close uk-alert-accept uk-button uk-button-small">close</a>
         <p>Cookies help us provide, improve, and protect <?php bloginfo(); ?>. By using our services, you agree to our <a href="<?php echo esc_url(site_url('privacy-policy')); ?>">cookie policy</a>.</p>
